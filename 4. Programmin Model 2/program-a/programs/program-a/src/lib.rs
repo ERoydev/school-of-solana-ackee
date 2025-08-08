ba346b6c9962c9785a4c937fe9c0d73b7de809fe
ERoydev <e.roydev@gmail.com>
2025-08-08 11:16:17 +0300

    video program model 2 finished

--- 4. Programmin Model 2/program-a/programs/program-a/src/lib.rs
+++ 4. Programmin Model 2/program-a/programs/program-a/src/lib.rs
@@ -5,26 +5,45 @@ declare_id!("4XZ7uQyeUMprrAX4qv3otAMzHMSkk76dajMgTStmo6t5");
 
 #[program]
 pub mod program_a {
-    use anchor_lang::solana_program::{instruction::Instruction, program::invoke_signed};
+    use anchor_lang::{system_program::{transfer, Transfer}};
+    
 
     use super::*;
 
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
         msg!("Greetings from program A");
+        let from_pubkey = ctx.accounts.pda_account.to_account_info();
+        let to_pubkey = ctx.accounts.signer.to_account_info();
+        let program_id = ctx.accounts.system_program.to_account_info();
+
+        let signer_address = ctx.accounts.signer.key();
 
         // We need PDA account to sign that CPI, because we need to authorize the PDA ACCOUNT
         // Because we want to transfer lamports from the PDA account to the `signer`
-        let program_id = ctx.accounts.system_program.to_account_info();
-        let account_metas = ctx.accounts.to_account_metas(Some(true));
+        let bump_seed = ctx.bumps.pda_account;
+        let signer_seeds: &[&[&[u8]]] = &[&[b"ackee",signer_address.as_ref(),&[bump_seed]]];
 
-        let instruction = Instruction {
-            program_id: program_id.key(),
-            accounts: account_metas,
-            data: []
-        }
+        let transfer_cpi = CpiContext::new(
+            program_id,
+            Transfer {
+                from: from_pubkey.clone(),
+                to: to_pubkey,
+            }
+        )
+        .with_signer(signer_seeds);
 
-        invoke_signed(instruction, account_infos, signers_seeds);
+        transfer(transfer_cpi, 1_000_000)?;
 
+        // So i just make a CPI to a programB to invoke the instruction from it
+        let cpi_context = CpiContext::new_with_signer( // new_with_signer() allows us to specify the signer seeds in order to sing the CPI for the PDA account
+            ctx.accounts.program_b.to_account_info(),
+            program_b::cpi::accounts::Initialize{ pda_account: from_pubkey }, 
+            signer_seeds
+        );
+
+        // If i use just ::new() the PDA won't be marked as a signer
+
+        program_b::cpi::initialize(cpi_context)?;
 
         Ok(())
     }
@@ -34,6 +53,7 @@ pub mod program_a {
 pub struct Initialize<'info> {
     // AccountInfo -> Does not specify any checks
     // Account -> anchor checks that the owner is my program, data can be serialized into structure and so on
+    /// CHECK: This PDA is derived deterministically using seeds [b"ackee", signer.key()] and verified by Anchor's seeds constraint. Safe to use as AccountInfo since we only need it for signing CPI calls, not reading/writing structured data.
     #[account(
         mut,
         seeds = [b"ackee", signer.key().as_ref()],
--- 4. Programmin Model 2/program-a/tests/program-a.ts
+++ 4. Programmin Model 2/program-a/tests/program-a.ts
@@ -1,16 +1,62 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { ProgramA } from "../target/types/program_a";
+import { ProgramB } from "../target/types/program_b";
+import { assert } from "chai";
+
+const PDA_ACCOUNT_SEED = "ackee";
 
 describe("program-a", () => {
   // Configure the client to use the local cluster.
   anchor.setProvider(anchor.AnchorProvider.env());
 
-  const program = anchor.workspace.programA as Program<ProgramA>;
+  const programA = anchor.workspace.programA as Program<ProgramA>;
+  const programB = anchor.workspace.programB as Program<ProgramB>; // Logically i should create a new programB instance to use it in the test. since i have CPI call to program B in the program A.
+
+  let signer = anchor.web3.Keypair.generate();
 
   it("Is initialized!", async () => {
     // Add your test here.
-    const tx = await program.methods.initialize().rpc();
+    let connection = programA.provider.connection;    
+    const [pda_address, bump] = anchor.web3.PublicKey.findProgramAddressSync(
+      [
+        // Here i specify all the seeds that I used in the program
+        Buffer.from(PDA_ACCOUNT_SEED), 
+        signer.publicKey.toBuffer()
+      ], programA.programId // programId is part of derivation process its Program Derived Address after all.
+    );
+    
+    await airdrop(connection, pda_address, 500_000_000_000);
+
+    const tx = await programA.methods.initialize()
+      .accounts({
+        pdaAccount: pda_address,
+        signer: signer.publicKey,
+        systemProgram: anchor.web3.SystemProgram.programId, 
+        programB: programB.programId // CPI call to program B 
+      })
+      .signers([signer])
+      .rpc();
+    
     console.log("Your transaction signature", tx);
+
+    // I use .getAccountInfo to fetch the account info and i can whech whatever i want then 
+    let pda_address_info = await connection.getAccountInfo(pda_address);
+    let signer_info = await connection.getAccountInfo(signer.publicKey);
+
+    assert.equal(pda_address_info.lamports, 499999000000);
+    assert.equal(signer_info.lamports, 1_000_000);
   });
 });
+
+
+export async function airdrop(
+  connection: any,
+  address: any,
+  amount = 500_000_000_000 // 0.5 SOL`
+) {
+  await connection.confirmTransaction(
+    await connection.requestAirdrop(address, amount),
+    "confirmed"
+  );
+}
